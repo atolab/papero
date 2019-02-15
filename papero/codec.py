527fc4ad6f939cb8af815cefd8820691cfdd534c
gabrik <gabriele.baldoni@gmail.com>
2019-02-15 10:22:38 +0100

    fix bug on decoding of empty list of properties or property with empty value
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- papero/codec.py
+++ papero/codec.py
@@ -1,20 +1,21 @@
 # This file contains all message codec necessary
-# for the client. Notice that it does not necessarily 
-# provide both encoder and decored for all messages since 
+# for the client. Notice that it does not necessarily
+# provide both encoder and decored for all messages since
 # in some cases messages will either only be sent or only received.
 
 import logging
-from .property import Property 
+from .property import Property
 
 
 def encode_property(buf, p):
     buf.put_string(p.key)
-    buf.put_string(p.value)    
+    buf.put_string(p.value)
+
 
 def decode_property(buf):
     k = buf.get_string()
     v = buf.get_string()
-    return Property(k,v)
+    return Property(k, v)
 
 
 def encode_sequence(buf, xs, encoder):
@@ -22,12 +23,14 @@ def encode_sequence(buf, xs, encoder):
     for x in xs:
         encoder(buf, x)
 
-def  decode_sequence(buf, decoder):    
+
+def decode_sequence(buf, decoder):
     n = buf.get_vle()
     xs = []
     for _ in range(0, n):
         xs.append(decoder(buf))
     return xs
+
 
 def encode_properties(buf, ps):
     s = ''
@@ -39,12 +42,16 @@ def encode_properties(buf, ps):
                 s = s + ';'
         buf.put_string(s)
 
+
 def decode_properties(buf):
     ps = []
     s = buf.get_string()
+    if len(s) == 0:
+        return ps
     s = s.split(';')
     for ss in s:
         p = ss.split('=')
+        if len(p) == 1:
+            p.append('')
         ps.append(Property(p[0], p[1]))
     return ps
-    
